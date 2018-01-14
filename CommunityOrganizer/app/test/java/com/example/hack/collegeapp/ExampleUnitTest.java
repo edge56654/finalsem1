55a1a3c0b2a3c05d49e2cf238aafab122e90ab59
hannah edge <30994556+edge56654@users.noreply.github.com>
2018-01-14 11:23:08 -0800

    Update ExampleUnitTest.java

--- CommunityOrganizer/app/test/java/com/example/hack/collegeapp/ExampleUnitTest.java
+++ CommunityOrganizer/app/test/java/com/example/hack/collegeapp/ExampleUnitTest.java
@@ -4,14 +4,18 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-}
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestJunit.class);
+		
+      for (Failure failure : result.getFailures()) {
+         System.out.println(failure.toString());
+      }
+		
+      System.out.println(result.wasSuccessful());
+   }
+}  
